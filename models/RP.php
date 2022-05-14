016df02cb3f0c1b0616e968eee8bdc4dccc26e70
MMDIOUF <93401353+MMDIOUF@users.noreply.github.com>
2022-05-14 13:57:38 +0000

    Update RP.php

--- models/RP.php
+++ models/RP.php
@@ -1,21 +1,18 @@
 <?php 
-//RP herite de User
-class RP extends User{
-   
+namespace App\Models;
+class RP extends User {
+    
     public function __construct()
     {
-        $this->role="ROLE_RP";
+        parent::__construct();
+        parent::$role="ROLE_RP";
+        
     }
-    //Redefinition => evolution
-      //1-Heritage de Methode
-      //2-Redefinir=> changer son comportement
-       /**
-       * Set the value of role
-       *
-       * @return  self
-       */ 
-       public function setRole($role)
-        {
-            return $this;
-        }
+
+    public static  function selectAll(){
+        $sql="select *  from  ".parent::$table." where role like ? ";
+       return parent::database()->executeSelect($sql,[parent::$role]);
+     }
+
+
 }
