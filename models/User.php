ad61107c225aeb5e9dedcb00ca3f8f2c73c16d2c
MMDIOUF <93401353+MMDIOUF@users.noreply.github.com>
2022-05-14 13:55:06 +0000

    Update User.php

--- models/User.php
+++ models/User.php
@@ -1,102 +1,100 @@
 <?php 
-//Classe Concrete lorsqu'elle est instanciable 
-//Classe Abstraire lorsqu'elle n'est pas instanciable 
-//Classe final => sterile lorsqu'elle 
-  //ne participe pas dans hierarchie heritage
-abstract class User{
-    //Atributs Instances
-      protected int $id;
-      protected string $login;
-      protected string $password;
-      protected string $role;
-      
-     //Methodes
-     //Constructeur par defaut
-     public function __construct()
+
+ namespace App\Models;
+ use App\Core\Model;
+abstract class User extends Model{
+    //Attributs 
+     protected int   $id;
+     protected string $login;
+     protected string $password;
+     protected static string $role;
+
+    //Methodes
+    //Constructeur
+    public function __construct()
+    {
+       parent::$table="user" ;
+    }
+    //Getters => Obtenir la valeur d'un attribut private ou protected
+                //a partir de l'interface de la classe
+    public function getId():int{
+       return $this->id;
+    }
+
+    //Setters=> Modifie la valeur d'un attribut private ou protected
+                //a partir de l'interface de la classe
+    public function setId(int $id ):void{
+               $this->id=$id;   
+    }
+    
+
+     /**
+      * Get the value of login
+      */ 
+     public function getLogin()
      {
+          return $this->login;
+     }
+
+     /**
+      * Set the value of login
+      *
+      * @return  self
+      */ 
+     public function setLogin($login)
+     {
+          $this->login = $login;
+
+          return $this;
+     }
+
+     /**
+      * Get the value of password
+      */ 
+     public function getPassword()
+     {
+          return $this->password;
+     }
+
+     /**
+      * Set the value of password
+      *
+      * @return  self
+      */ 
+     public function setPassword($password)
+     {
+          $this->password = $password;
+
+          return $this;
+     }
+
+     /**
+      * Get the value of role
+      */ 
+     public function getRole()
+     {
+          return $this->role;
+     }
+
+     /**
+      * Set the value of role
+      *
+      * @return  self
+      */ 
+     public function setRole($role)
+     {
+          $this->role = $role;
+
+          return $this;
+     }
+
+     public function insert(){
          
-     } 
-     //Getters
-
-      /**
-       * Get the value of id
-       */ 
-      public function getId()
-      {
-            return $this->id;
-      }
-
-      /**
-       * Get the value of login
-       */ 
-      public function getLogin()
-      {
-            return $this->login;
-      }
-
-      /**
-       * Get the value of password
-       */ 
-      public function getPassword()
-      {
-            return $this->password;
-      }
-
-      /**
-       * Get the value of role
-       */ 
-      public function getRole()
-      {
-            return $this->role;
-      }
-
-      
-
-      /**
-       * Set the value of id
-       *
-       * @return  self
-       */ 
-      public function setId($id)
-      {
-            $this->id = $id;
-
-            return $this;
-      }
-
-      /**
-       * Set the value of login
-       *
-       * @return  self
-       */ 
-      public function setLogin($login)
-      {
-            $this->login = $login;
-
-            return $this;
-      }
-
-      /**
-       * Set the value of password
-       *
-       * @return  self
-       */ 
-      public function setPassword($password)
-      {
-            $this->password = $password;
-
-            return $this;
-      }
-
-      /**
-       * Set the value of role
-       *
-       * @return  self
-       */ 
-      public  function setRole($role)
-      {
-            $this->role = $role;
-
-            return $this;
-      }
+          //die(parent::$table);
+          $sql="INSERT INTO  ".parent::$table."  (`login`, `password`,  `role`)
+               VALUES ( ?, ?, ?);";
+              
+         return parent::database()->executeUpdate($sql,[
+                                                  $this->login,$this->password,self::$role]);
+     }
 }
