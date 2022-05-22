9045e337db79bf9063311d7b091918890c3697b2
MMDIOUF <93401353+MMDIOUF@users.noreply.github.com>
2022-05-22 23:27:23 +0000

    Delete Imodel.php

--- models/Imodel.php
+++
@@ -1,8 +0,0 @@
-<?php
-interface IModel{
-    public function insert();
-    public function update();
-    public static function selectAll();
-    public static function delete(int $id);
-    public static function selectById(int $id);
-}
