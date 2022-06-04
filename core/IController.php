4dcdf91a056a60270f9ca7df10a519832b0f7360
MMDIOUF <93401353+MMDIOUF@users.noreply.github.com>
2022-06-04 18:47:19 +0000

    Delete core directory

--- core/IController.php
+++
@@ -1,8 +0,0 @@
-<?php 
-namespace App\Core;
-interface IController{
-    public function lister();
-    public function ajouter();
-    public function supprimer();
-    public function modifier();
-}
