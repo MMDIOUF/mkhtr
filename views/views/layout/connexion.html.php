7997ac0ff2891fe109e01eacc771a190245fb94e
MMDIOUF <93401353+MMDIOUF@users.noreply.github.com>
2022-06-04 18:33:11 +0000

    Delete views/views directory

--- views/views/layout/connexion.html.php
+++
@@ -1,4 +0,0 @@
-<?php    require_once(ROOT."views/partials/header.html.php"); ?>
-                      <?=$content_for_views?>
-   
-<?php    require_once(ROOT."views/partials/footer.html.php"); ?>
