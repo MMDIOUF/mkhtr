fd707aa150f6033377be26a941b17401fbbb22f4
MMDIOUF <93401353+MMDIOUF@users.noreply.github.com>
2022-06-04 18:35:29 +0000

    Update liste.html.php

--- views/classe/liste.html.php
+++ views/classe/liste.html.php
@@ -1,13 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?=WEB_URL?>css/style.css">
-    <title>Document</title>
-</head>
-<body>
-    <h2>Liste des Classes</h2>
-</body>
-</html>
+
+
+<div class="card border-light my-3 w-75 shadow   " >
+  <div class="card-header bg-transparent border-light bg-header">Liste des Classe</div>
+    <div class="card-body ">
+          <div class="row d-flex justify-content-end">
+              <div class="col-2 mb-1 ">
+                  <a name="" id="" class="btn btn-outline-info text-dark btn-xs" href="<?=WEB_URL?>/classe-add" role="button">Nouveau</a>
+              </div>
+          </div>
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>Filiere</th>
+                    <th>Niveau</th>
+                    <th>Libelle</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php foreach($classes as $value):?>
+                <tr>
+                    <td  style="width:15%"><?=$value['filiere']?></td>
+                    <td  style="width:15%"><?=$value['niveau']?></td>
+                    <td  style="width:25%"><?=$value['libelle']?></td>
+                    <td  style="width:45%">
+                        <a name="" id="" class="btn btn-outline-success text-dark btn-xs" href="#" role="button">Details</a>
+                        <a name="" id="" class="btn btn-outline-warning text-dark btn-xs" href="#" role="button">Edition</a>
+                        <a name="" id="" class="btn btn-outline-danger text-dark" href="#" role="button">Supprimer</a>
+                    </td>
+                </tr>
+            <?php endforeach?>
+            </tbody>
+        </table>
+        
+                       
+    </div>
+                   
+</div>
